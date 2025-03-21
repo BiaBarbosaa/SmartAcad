b7681d761a9281a032e4860e2c8054bf8fdb94e5
Beatriz Bucher <beatriz.pessoa033@gmail.com>
2025-03-21 07:36:44 -0300

    corrigido o conflito

--- React/start/src/App.js
+++ React/start/src/App.js
@@ -1,12 +1,7 @@
 import Home from "./components/paginas/home/Home";
 import Login from "./components/paginas/Login/Login";
 import NotFound from "./components/paginas/404/NotFound";
-<<<<<<< HEAD
-=======
-import CadastrarAluno from "./components/paginas/aluno/CadastrarAluno";
-import ListarAluno from "./components/paginas/aluno/ListarALuno";
-import EditarAluno from "./components/paginas/Login/Login";
->>>>>>> c0b2b06cab1952100675ffb2503a5f35fd325ada
+
 import { Route, Routes } from "react-router-dom";
 import AlternadorDeTema from  "./components/AlternadorDeTema"
 import CadastrarCliente from "./components/paginas/cliente/CadastrarCliente"
@@ -20,13 +15,11 @@ import EditarContrato from "./components/paginas/contrato/EditarContrato"
 
 
 
-
 function App() {
   return (
     <>
         <AlternadorDeTema />
         <Routes>
-<<<<<<< HEAD
           <Route path="/listar-cliente" element={<ListarCliente/>} />
           <Route path="/cadastrar-cliente" element={<CadastrarCliente/>} />
           <Route path="/editar-cliente/:id" element={<EditarCliente/>} />
@@ -34,12 +27,8 @@ function App() {
           <Route path="/listar-contrato" element={<ListarContrato/>} />
           <Route path="/cadastrar-contrato" element={<CadastrarContrato/>} />
           <Route path="/editar-contrato/:id" element={<EditarContrato/>} />
-=======
           <Route path="/login" element={<Login/>} />
-          <Route path="/listar-aluno" element={<ListarAluno/>} />
-          <Route path="/cadastrar-aluno" element={<CadastrarAluno/>} />
-          <Route path="/editar-aluno/:id" element={<EditarAluno/>} />
->>>>>>> c0b2b06cab1952100675ffb2503a5f35fd325ada
+          
           {/*Rota para página 404*/}
           <Route path="*" element={<NotFound/>} />
           {/*Rota para página home*/}
--- React/start/src/components/paginas/cliente/CadastrarCliente.jsx
+++ React/start/src/components/paginas/cliente/CadastrarCliente.jsx
@@ -1,17 +1,12 @@
-
-
 import './cliente.css'
 import Navbar from '../../template/Navbar';
 import Sidebar from '../../template/Sidebar';
 import MainCadastroCliente from '../../MainCadastroCliente'
 
-<<<<<<< HEAD:React/start/src/components/paginas/cliente/CadastrarCliente.jsx
 function CadastrarCliente() {
-=======
-function                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            EditarAluno() {
->>>>>>> c0b2b06cab1952100675ffb2503a5f35fd325ada:React/start/src/components/paginas/aluno/EditarAluno.jsx
-    return (
-        <>
+
+return(
+<>
             <Navbar />
             <div className="container-fluid">
                 <div className="row">
@@ -21,6 +16,7 @@ function                                                                        
                 </div>
             </div>
         </ >
-    );
+);
+
 }
 export default CadastrarCliente;
