b43a619814f61fa048afdf004958239ade93ece1
Bia Barbosa <itsbeatrizbarbosa@gmail.com>
2025-03-28 09:15:41 -0300

    alterações na cliente cadastrar

--- React/start/src/components/paginas/cliente/CadastrarCliente.jsx
+++ React/start/src/components/paginas/cliente/CadastrarCliente.jsx
@@ -5,11 +5,7 @@ import Navbar from '../../template/Navbar';
 import Sidebar from '../../template/Sidebar';
 import MainCadastroCliente from '../../MainCadastroCliente'
 
-<<<<<<< HEAD:React/start/src/components/paginas/cliente/CadastrarCliente.jsx
-function CadastrarCliente() {
-=======
-function                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            EditarAluno() {
->>>>>>> c0b2b06cab1952100675ffb2503a5f35fd325ada:React/start/src/components/paginas/aluno/EditarAluno.jsx
+function CadastrarCliente() {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            EditarAluno() {
     return (
         <>
             <Navbar />
@@ -22,5 +18,6 @@ function                                                                        
             </div>
         </ >
     );
+}
 }
 export default CadastrarCliente;
